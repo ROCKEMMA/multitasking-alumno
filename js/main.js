ca2b0fafb4a1801dc6080aaf7e0aa62c2c625011
David <jose.aceituno.balconi@gmail.com>
2024-09-04 11:06:02 -0600

    diseno de header terminado

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-let DOM=document.querySelector('#DOM')
-DOM.innerHTML=``;
+import { header } from "../componentes/header/header.js";
+let DOM=document.querySelector('#root')
+DOM.innerHTML=`
+ <div id="main">
+    <header id="header">${header}</header>
+    <main></main>
+    <footer></footer>
+ </div>
+`;
 
 console.log('hola mundo');
