3702529697d4a593b00869eb1daeab682d3f6aaf
David <jose.aceituno.balconi@gmail.com>
2024-09-09 19:39:18 -0600

    correccion

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-import { header } from "./componentes/header/header.js";
+import { headerComponent } from "./componentes/header/header.js"
 let DOM=document.querySelector('#root')
-DOM.innerHTML=`
- <div id="main">
-    <header id="header">${header}</header>
-    <main></main>
-    <footer></footer>
- </div>
-`;
-
-console.log('hola mundo');
+DOM.appendChild(headerComponent)
