ab2170d3bed635bf87ce84ad3475d0f79132c831
David <jose.aceituno.balconi@gmail.com>
2024-10-10 20:30:54 -0600

    multiTasking

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import { headerComponent } from "./componentes/header/header.js"
-import { seccionTareas } from "./componentes/componente/itemTarea/itemTarea.js"
-let DOM=document.querySelector('#root')
-DOM.appendChild(headerComponent)
-DOM.appendChild(seccionTareas)
-    
+import { headerComponent } from "./componentes/header/header.js";
+import { seccionTareas } from "./componentes/componente/itemTarea/itemTarea.js";
 
-export {DOM}
+let DOM = document.querySelector('#root');
+DOM.appendChild(headerComponent);
+DOM.appendChild(seccionTareas);
+
+export { DOM };
